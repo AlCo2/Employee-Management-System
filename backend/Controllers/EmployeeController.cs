79d99cce9a97a2b4564c46b73ec2f9adc07ad097
Alco2 <abdou.lahboub.wwe@gmail.com>
2024-07-08 17:14:40 +0100

    init Employee model and controller

---
+++ backend/Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Employee_Management_System.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class EmployeeController : ControllerBase
+	{
+		private readonly EmployeeStoreContext _context;
+		public EmployeeController(EmployeeStoreContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			var employees = _context.Employees.ToList();
+			return Ok(employees);
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult Get([FromRoute] int id)
+		{
+			var employee = _context.Employees.Find(id);
+			if (employee == null)
+			{
+				return NotFound();
+			}
+			return Ok(employee);
+		}
+	}
+}
---
+++ backend/EmployeeStoreContext.cs
@@ -0,0 +1,21 @@
+﻿using Employee_Management_System.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee_Management_System
+{
+	public class EmployeeStoreContext : DbContext
+	{
+		protected readonly IConfiguration Configuration;
+
+		public EmployeeStoreContext(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlite(Configuration.GetConnectionString("WebApiDatabase"));
+		}
+
+		public DbSet<Employee> Employees { get; set; }
+	}
+}
